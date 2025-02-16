7a7e45894a97aad4913d0e85ecfec092aadcdef7
Aman Mishra <amanmishra.5272@gmail.com>
2025-02-16 13:37:48 +0530

    Enhance Contact page; add store information, careers section, and newsletter subscription box

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets.js";
+import NewsLetterBox from "../components/NewsLetterBox.jsx";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
 
-export default Contact
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          className="w-full md:max-w-[480px]"
+          src={assets.contact_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className=" text-gray-500">
+            Sector 93 Noida <br /> GB Nagar, UTTAR PRADESH, INDIA
+          </p>
+          <p className=" text-gray-500">
+            Tel: (+91) 1234567890 <br /> Email: support@RatnaRoyals.in
+          </p>
+          <p className="font-semibold text-xl text-gray-600">
+            Careers at Forever
+          </p>
+          <p className=" text-gray-500">
+            Learn more about our teams and job openings.
+          </p>
+          <button class="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+
+      <NewsLetterBox />
+    </div>
+  );
+};
+
+export default Contact;
