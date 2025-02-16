70a53752041d2a1c5e7aa27d2daaf3dfb8952182
Aman Mishra <amanmishra.5272@gmail.com>
2025-02-17 00:30:09 +0530

    Add Cloudinary configuration and integrate it into server setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import connectDB from "./config/mongoDB.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 // App Config
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
+connectCloudinary();
+
 // middlewears
 app.use(express.json());
 
