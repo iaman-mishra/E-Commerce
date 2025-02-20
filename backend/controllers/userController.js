5a76a3121c52994030898f9219d43514dcbeecf5
Aman Mishra <amanmishra.5272@gmail.com>
2025-02-21 00:08:27 +0530

    Implement admin login functionality with error handling and JWT token generation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -76,6 +76,21 @@ const registerUser = async (req, res) => {
   }
 };
 
-const adminLogin = async () => {};
+const adminLogin = async (req,res) => {
+
+  try {
+    const {email , password} = req.body;
+    if (email===process.env.ADMIN_EMAIL && password===process.env.ADMIN_PASSWORD) {
+      const token = jwt.sign(email+password, process.env.JWT_SECRET_KEY);
+      res.json({success:true,token})
+    }else{
+      res.json({success:false,message:"Invalid admin credentials"})
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error logging in admin" });
+  }
+
+};
 
 export { userLogin, registerUser, adminLogin };
